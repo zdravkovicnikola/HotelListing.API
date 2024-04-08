644ddd9447f6cfa77fdf73f780bfd4e48397b0f6
zdravkovicnikola <nzdravkovic2001@gmail.com>
2024-04-08 18:35:26 +0200

    Kreirane su migracije, baza podataka, same klase code first nacinom, promenjeno dosta toga

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+
+        [ForeignKey(nameof(CountryId))] // anotacija
+        public int CountryId { get; set; } // ovo vidi Baza
+        public Country Country { get; set; } // ovo vidi okruzenje
+
+
+    }
+}
